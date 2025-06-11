0965062d0aebe69d441f356b4af0e2cbae1b2688
Aisha Daodu <aishajumoke15@gmail.com>
2025-06-11 14:31:11 +0100

    New push

--- proptii-backend/src/app.module.ts
+++ proptii-backend/src/app.module.ts
@@ -1,28 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ViewingRequestModule } from './viewing-request/viewing-request.module';
-import { DatabaseModule } from './database/database.module';
-import { ReferencingModule } from './referencing/referencing.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database.module';
+import { ViewingRequestModule } from './modules/viewing-request.module';
+import { ReferencingModule } from './modules/referencing.module';
 import { SearchModule } from './search/search.module';
-import { SheetsModule } from './sheets/sheets.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 10,
-    }]),
     DatabaseModule,
     ViewingRequestModule,
     ReferencingModule,
     SearchModule,
-    SheetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
